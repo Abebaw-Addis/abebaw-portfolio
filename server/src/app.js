94147fa835f43e337bf7e5eca6801389a2853fbd
Abebaw-Addis <abadis1221@gmail.com>
2026-07-03 11:35:36 +0300

    debugging mongoDB connection on production and CORS

--- server/src/app.js
+++ server/src/app.js
@@ -10,7 +10,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        "https://abebaw-portfolio.onrender.com",
+        "http://localhost:5173"
+    ],
+    credentials: true
+}));
 app.use(helmet());
 app.use(morgan("dev"));
 
